6b54732314b93a2c3be317d71999ba978172deba
Fernando S'v <69124578+Akbal-21@users.noreply.github.com>
2020-12-08 14:00:55 -0600

    Update Main.py

--- Chees/Main.py
+++ Chees/Main.py
@@ -23,8 +23,8 @@ def main():
     screen.fill(p.Color("white"))
     gs = engine.GameState()
     validMoves = gs.getValidMoves()
-    moveMade = False   #bandera referencia
-
+    moveMade = False  # bandera referencia
+    animate = False
     loadImage()
     running = True
     sqSelected = ()  # cuadro no seleccionadao
@@ -46,30 +46,52 @@ def main():
                 if len(playerClicks) == 2:
                     move = engine.Move(playerClicks[0], playerClicks[1], gs.board)
                     print(move.getChessNotation())
-                    if move in validMoves:
-                        gs.makeMove(move)
-                        moveMade = True
-                        sqSelected = ()
-                        playerClicks = []
-                    else:
-                        playerClicks = [sqSelected]
-
+                    gs.makeMove(move)
+                    sqSelected = ()
+                    playerClicks = []
+                    animate = True
             elif i.type == p.KEYDOWN:
                 if i.key == p.K_z:
                     gs.undoMove()
                     moveMade = True
+                    animate = False
 
         if moveMade:
-            validMoves = gs.getValidMoves()
+            if animate:
+                animateMove(gs.moveLog[-1], screen, gs.board, clock)
+            #validMoves = gs.getValidMoves()
             moveMade = True
+            animate=False
 
+        # drawGameState(screen, gs, validMoves, sqSelected)
         drawGameState(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
 
 
+'''
+Selection squares
+'''
+
+
+def highlightSquares(screen, gs, validMoves, sqSelected):
+    if sqSelected != ():
+        r, c = sqSelected
+        if gs.board[r][c][0] == ("w" if gs.whitemove else "b"):
+            s = p.Surface((SQ_SIZE, SQ_SIZE))
+            s.set_alpha(100)
+            s.fill(p.Color("blue"))
+            screen.blit(s, (c * SQ_SIZE, r * SQ_SIZE))
+            s.fill(p.Color("yellow"))
+            for move in validMoves:
+                if move.starCol == r and move.starCol == c:
+                    screen.blit(s, (move.endCol * SQ_SIZE, move.endRow * SQ_SIZE))
+
+
+# def drawGameState(screen, gs, validMoves, sqSelected):
 def drawGameState(screen, gs):
     drawBoard(screen)
+    # highlightSquares(screen, gs, validMoves, sqSelected)
     drawPieces(screen, gs.board)
 
 
@@ -95,6 +117,30 @@ def drawPieces(screen, board):
                 screen.blit(IMAGES[piece], p.Rect(j * SQ_SIZE, i * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 
+'''Animation'''
+
+
+def animateMove(move, screen, board, clock):
+    global colors
+    dr = move.endRow - move.startRow
+    dc = move.endCol - move.starCol
+    framesPErSquare = 10
+    frameCount = (abs(dr) + abs(dc)) * framesPErSquare
+    for frame in range(frameCount):
+        r, c = (move.startRow + dr * frame / frameCount, move.starCol + dc * frame / frameCount)
+        drawBoard(screen)
+        drawPieces(screen, board)
+        color = colors[(move.endRow + move.endCol) % 2]
+        endSquare = p.Rect(move.endCol * SQ_SIZE, move.endRow * SQ_SIZE, SQ_SIZE, SQ_SIZE)
+        p.draw.rect(screen, color, endSquare)
+        if move.pieceCapture != "--":
+            screen.blit(IMAGES[move.pieceCapture], endSquare)
+        screen.blit(IMAGES[move.pieceMoved], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+        p.display.flip()
+        clock.tick(60)
+
+
 if __name__ == '__main__':
     main()
+
 
